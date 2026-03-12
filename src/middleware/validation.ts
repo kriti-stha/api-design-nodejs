82988bea63551836139b0b7cf864dbf515727d05
kritistha <kritistha020@gmail.com>
2026-03-12 11:38:20 +0545

    add params and query validation schemas

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -22,3 +22,44 @@ export const validateBody = (schema: ZodSchema) => {
     }
   }
 }
+
+export const validateParams = (schema: ZodSchema) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse(req.params)
+      next()
+    } catch (e) {
+      if (e instanceof ZodError) {
+        return res.status(400).json({
+          error: 'Invald Params',
+          details: e.issues.map((err) => ({
+            field: err.path.join('.'),
+            message: err.message,
+          })),
+        })
+      }
+      next(e)
+    }
+  }
+}
+
+
+export const validateQuery = (schema: ZodSchema) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse(req.query)
+      next()
+    } catch (e) {
+      if (e instanceof ZodError) {
+        return res.status(400).json({
+          error: 'Invald Query',
+          details: e.issues.map((err) => ({
+            field: err.path.join('.'),
+            message: err.message,
+          })),
+        })
+      }
+      next(e)
+    }
+  }
+}
--- src/routes/habitRoutes.ts
+++ src/routes/habitRoutes.ts
@@ -1,25 +1,39 @@
 import { Router } from 'express'
+import { validateBody, validateParams } from '../middleware/validation.ts'
+import z from 'zod'
+
+const createSchema = z.object({
+  name: z.string(),
+})
+
+const completeParamsSchema = z.object({
+  id: z.string(),
+})
 
 const router = Router()
 
 router.get('/', (req, res) => {
-  res.status(200).json({message: 'Successfully fetched habits'})
+  res.status(200).json({ message: 'Successfully fetched habits' })
 })
-
 
 router.get('/:id', (req, res) => {
-  res.status(200).json({message: 'GOT 1 HABIT'})
+  res.status(200).json({ message: 'GOT 1 HABIT' })
 })
 
-router.post('/', (req, res) => {
-  res.status(201).json({message: 'CREATED A HABIT'})
+router.post('/', validateBody(createSchema), (req, res) => {
+  res.status(201).json({ message: 'CREATED A HABIT' })
 })
 
-router.delete('/:id', (req, res) => {
-    res.json({message: 'deleted habit'})
-})
+router.delete(
+  '/:id',
+  validateParams(completeParamsSchema),
+  validateBody(createSchema),
+  (req, res) => {
+    res.json({ message: 'deleted habit' })
+  },
+)
 
 router.post('/:id/complete', (req, res) => {
-    res.status(201).json({message: 'Completed A HABIT'})
-  })
+  res.status(201).json({ message: 'Completed A HABIT' })
+})
 export default router
